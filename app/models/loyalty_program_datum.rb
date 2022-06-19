4acf9d4de57bdd7c4b2c3916986ee0d9f922cc21
kaguyasama12 <matthewchristopherpohadi@gmail.com>
2022-06-19 17:27:02 +0800

    modify loyalty program data and transaction model

--- app/models/loyalty_program_datum.rb
+++ app/models/loyalty_program_datum.rb
@@ -1,6 +1,30 @@
 class LoyaltyProgramDatum < ApplicationRecord
-    has_many: Transaction
+    has_many: Transaction, foreign_key: "loyalty_program_id"
     serialize :transaction_history, Array
+
+
+    def update_amount
+        # Add amount based on loyalty program id
+
+        new_amount = transactions.where(loyalty_program_id: loyalty_program_id).sum(:amount)
+        
+        #Assign this new ammount to LoyaltyProgramDatum that has the same loyalty_program_id
+        self.points = new_amount
+
+        save
+
+    end
+
+    def browse_transaction_history
+        transaction_history
+    end
+
+
 
     
 end
+
+
+
+
+
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,17 @@
 class Transaction < ApplicationRecord
-    belongs_to: LoyaltyProgramDatum
+    attr_readonly :amount, :loyalty_program_id
+
+    validates :amount
+
+
+
+
+    belongs_to: LoyaltyProgramDatum,
+
+
+    after_create do
+        LoyaltyProgramDatum.update_amount
+    end
 
     
 end
