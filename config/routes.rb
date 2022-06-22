c2d06ed854570a333f14afea860194936aea2f3d
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-06-22 15:32:08 +0800

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
   resources :loyalty_programs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
