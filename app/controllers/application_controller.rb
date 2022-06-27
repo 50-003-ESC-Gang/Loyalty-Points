0863c4e173132417bf28cb0b48cbfd2d3d35b706
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-06-27 10:43:26 +0800

    added landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+
     # helper_method :current_user
 
     # def current_user
@@ -6,4 +8,9 @@ class ApplicationController < ActionController::Base
     # rescue
     #     nil
     # end
+
+  # def after_sign_in_path_for(user)
+  #   # your path goes here
+  #   user_posts_path(user) # as an example
+  # end
 end
