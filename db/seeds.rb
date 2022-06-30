cdf6e0a88cfa4be9ce616da93bd2cafe62423d4d
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-06-30 11:13:36 +0800

    remove tailwindcss

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@
 
 
     #Trans
-    txn = Transaction.create(loyalty_program_data_id: i, amount: i, date: "2000-01-01 02:00:00", status: 0, receiver: i - 1)
+    txn = Transaction.create(loyalty_program_data_id: i, amount: i, date: "2000-01-01 02:00:00", status: 0, receiver: i)
 
     
 
