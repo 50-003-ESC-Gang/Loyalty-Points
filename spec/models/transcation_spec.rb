d945efc1f1b3e7508a140aea7aaf0bfddc3ba7a7
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-07-01 01:13:48 +0800

    fixed spec

--- spec/models/transcation_spec.rb
+++ spec/models/transcation_spec.rb
@@ -18,12 +18,12 @@ RSpec.describe Transcation, type: :model do
     context 'when transcation is successful' do
       xit 'should create a transcation' do
         # transcation = create_loyalty_program_data()
-      xit 'should decrease the points of the sender' do
-        # Loyalty Program data in account for sending lp should decrease
-      end
-      xit 'should increase the points of the receiver' do
-        # Loyalty Program data in account for sending lp should increase
-
+        xit 'should decrease the points of the sender' do
+          # Loyalty Program data in account for sending lp should decrease
+        end
+        xit 'should increase the points of the receiver' do
+          # Loyalty Program data in account for sending lp should increase
+        end
       end
     end
   end
