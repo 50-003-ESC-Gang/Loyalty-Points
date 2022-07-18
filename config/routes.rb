cb1c2c225598c21ae8e11baecd07901b650506f0
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-07-18 10:37:15 +0800

    added csv_processing

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
 
   resources :transactions
 
+  resources :csv_processing
+
+
+  
   resources :loyalty_programs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
