3406cfc79318da4ee2af00114406838e02f5cbd2
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-07-20 20:09:22 +0800

    fixed failing lpd test

--- spec/models/loyalty_program_datum_spec.rb
+++ spec/models/loyalty_program_datum_spec.rb
@@ -5,26 +5,29 @@ RSpec.describe LoyaltyProgramDatum, type: :model do
     FactoryBot.create(:user)
   end
 
+  let(:lp) do
+    FactoryBot.create(:loyalty_program)
+  end
+
   context 'when happy case for create loyalty_program_datum' do
     it 'should create a loyalty_program_datum' do
-      expect(LoyaltyProgramDatum.count).to eq(0)
-      lp = FactoryBot.create(:loyalty_program)
+      initial_count = LoyaltyProgramDatum.count
 
-      loyalty_program_data = LoyaltyProgramDatum.new(account_id: user.account.id,
-                                                     loyalty_program_id: LoyaltyProgram.first.id).save
-      expect(LoyaltyProgramDatum.count).to eq(1)
+      LoyaltyProgramDatum.create(account_id: user.account.id,
+                                 loyalty_program_id: lp.id)
+      expect(LoyaltyProgramDatum.count).to eq(initial_count + 1)
     end
   end
 
   # TODO: Refer to https://github.com/50-003-ESC-Gang/Loyalty-Points/issues/86
-  context 'when loyalty program is created' do
-    xit 'should create a loyalty program data for user' do
-      expect(LoyaltyProgramDatum.count).to eq(0)
-      debugger
-      FactoryBot.create(:loyalty_program)
+  # context 'when loyalty program is created' do
+  #   xit 'should create a loyalty program data for user' do
+  #     expect(LoyaltyProgramDatum.count).to eq(0)
+  #     debugger
+  #     FactoryBot.create(:loyalty_program)
 
-      expect(LoyaltyProgramDatum.count).to eq(1)
-      expect(LoyaltyProgramDatum.first.loyalty_program_id).to eq(lp.loyalty_program_id)
-    end
-  end
+  #     expect(LoyaltyProgramDatum.count).to eq(1)
+  #     expect(LoyaltyProgramDatum.first.loyalty_program_id).to eq(lp.loyalty_program_id)
+  #   end
+  # end
 end
