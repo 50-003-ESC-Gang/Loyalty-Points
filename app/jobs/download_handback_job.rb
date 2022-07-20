ffc49f17ef3ea4ad208efcb79fade0b11ae625d8
Liu Lingao <lingaoliu105@gmail.com>
2022-07-20 20:46:12 +0800

    save generated files in tmp folder

--- app/jobs/download_handback_job.rb
+++ app/jobs/download_handback_job.rb
@@ -3,11 +3,13 @@ class DownloadHandbackJob < ApplicationJob
 
   def perform(*args)
     # Do something later
+    # args[0] is handback file name, args[1] is the folder
     puts "perform download"
     system "cmdftp -s:drivehq_retrieve.txt -%filename:#{args[0]}"
     puts "download ended"
     # TODO: move the downloaded file to desinated folder
-    Thread.new (AccrualProcessor.process_handback("./#{args[0]}"))
+    File.rename "./#{args[0]}", "#{args[1]}#{args[0]}"
+    Thread.new (AccrualProcessor.process_handback("#{args[1]}#{args[0]}"))
   end
 
 end
