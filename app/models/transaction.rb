48435b2b1a033231b09c736fbe3eacaa742992fd
Liu Lingao <lingaoliu105@gmail.com>
2022-07-20 09:44:38 +0800

    save

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,11 +3,10 @@ class Transaction < ApplicationRecord
 
   validate :amount
 
-  # belongs_to :loyalty_program_data, class_name: "loyalty_program_datum", foreign_key: "loyalty_program_datum_id"
+  belongs_to :loyalty_program_data, class_name: "loyalty_program_datum", foreign_key: "loyalty_program_datum_id"
   # belongs_to :receiver_data, class_name: "loyalty_program_datum", foreign_key: "receiver"
 
   has_one :user, through: :account, source: :index_account_on_user_id
-
   enum :status, %i[pending success failed cancelled]
 
   #to generate accrual, for testing purpose only
