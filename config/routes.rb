118449ba0ab67badc251ef520d397b64c2bb2f6c
Liu Lingao <lingaoliu105@gmail.com>
2022-07-20 13:38:43 +0800

    file can be generated from console

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,8 @@ Rails.application.routes.draw do
   end
 
 
-  resources :transactions
+  # resources :transactions
 
-  
-
-
-  
   resources :loyalty_programs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
