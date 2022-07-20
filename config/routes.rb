8f666bce2b275b82e7fa5611004694b34f37c00b
Liu Lingao <lingaoliu105@gmail.com>
2022-07-20 13:50:47 +0800

    routing isssue

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
 
-  # resources :transactions
+  resources :transactions
 
   resources :loyalty_programs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
