c1a91241e4a35eec1b49c378aff1c3bd95006dd9
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-07-22 11:49:05 +0800

    integration with signin

--- spec/controllers/loyalty_program_data_controller_spec.rb
+++ spec/controllers/loyalty_program_data_controller_spec.rb
@@ -1,19 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe LoyaltyProgramDataController, type: :controller do
-  
-  let(:user) {FactoryBot.create(:user)}
-  let(:lp) {FactoryBot.create(:loyalty_program)}
+  let(:user) { FactoryBot.create(:user) }
+  let(:lp) { FactoryBot.create(:loyalty_program) }
+  let(:lpd) { FactoryBot.create(:loyalty_program_datum, account_id: user.id, loyalty_program_id: lp.id) }
 
+  describe '#index' do
+    it 'should return loyalty program data for user account' do
+      sign_in user
+
+      get :index, params: { id: user.id }
+      expect(response).to have_http_status(200)
+    end
+  end
 
   describe '#update' do
-    it 'can be created' do
-      expect(user).to be_valid
-      expect(User.count).to eq(1)
-      expect(User.first.name).to eq(user.name)
+    context 'when happy case' do
+      xit 'should update loyalty program data points' do
+      end
     end
   end
 
   # TODO : still not too sure how to write test to test account creation
-  
 end
