e12de4d434ddbb83bc48eeb741b7bdb41d6cdb4f
ExtremelySunnyYk <chiayongkang@hotmail.com>
2022-08-05 08:03:39 +0800

    cleanup factories spec

--- spec/factories/loyalty_program_datum.rb
+++ spec/factories/loyalty_program_datum.rb
@@ -1,7 +1,6 @@
-# FactoryBot.define do
-#     FactoryBot.create(:loyalty_program)
-#     factory :loyalty_program_datum do
-#         loyalty_program_id{"id1"}
-#         account_id{1}
-#     end
-# end
+FactoryBot.define do
+  factory :loyalty_program_datum do
+    loyalty_program
+    account
+  end
+end
